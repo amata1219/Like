ee417dd91102afb3cabff26bbdb180f18a8b973e
amata1219 <suzukiamata@gmail.com>
2019-12-11 19:46:41 +0900

    こみっとー

--- src/amata1219/like/text/Text.java
+++
@@ -1,5 +0,0 @@
-package amata1219.like.text;
-
-public class Text {
-
-}
--- src/amata1219/like/ui/MyLikeManagementUI.java
+++ src/amata1219/like/ui/MyLikeManagementUI.java
@@ -1,7 +1,6 @@
 package amata1219.like.ui;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
@@ -33,8 +32,11 @@ public class MyLikeManagementUI implements InventoryUI {
 	 * Math.min(size - (p x 45), 45) % 9
 	 * 
 	 * ---------
-	 * @---@---@
-	 * 45, 49, 53
+	 * @-@@-@@-@
+	 * 45,
+	 * 47,48
+	 * 50,51
+	 * 53
 	 * 
 	 */
 	
@@ -54,7 +56,7 @@ public class MyLikeManagementUI implements InventoryUI {
 		final Type where = Type.where(index, likes);
 		likes.sort(order.comparator);
 		return build(Math.min(likes.size() - (index * 45), 45), (p, l) -> {
-			l.title = Text.of("My Like Management @ %s").format(index + 1).colored();
+			l.title = Text.of("My Likes @ %s").apply(index + 1).get();
 			
 			l.defaultSlot(s -> {
 				s.icon(i -> {
@@ -63,37 +65,40 @@ public class MyLikeManagementUI implements InventoryUI {
 				});
 			});
 			
-			int start = Math.min(index * 45, likes.size() - 1);
-			IntStream.range(start, Math.min(start + 45, likes.size())).forEach(slotIndex -> {
+			int start = index * 45;
+			int remainder = likes.size() % 45;
+			IntStream.range(start, start + (remainder == 0 ? 45 : remainder)).forEach(slotIndex -> {
 				l.put(s -> {
 					Like like = likes.get(slotIndex);
 					s.icon(i -> {
 						i.material = config.icon(IconType.LIKE);
 						i.displayName = " ";
 						i.lore(
-							Text.of("&7-%s").format(like.description()).colored(),
-							"",
-							Text.of("&7-作成者: &a-%s").format(UUIDConverter.getNameFromUUID(like.owner())).colored(),
-							Text.of("&7-お気に入り数: &a-%s").format(like.favorites()).colored(),
-							Text.of("&7-作成日時: &a-%s").format(like.creationTimestamp()).colored(),
-							Text.of("&7-ワールド: &a-%s").format(config.alias(like.world()).or(() -> "Unknown")).colored(),
-							Text.of("&a-座標-&7-: &f-X-&7-: &f-%s Y-&7-: &f-%s Z-&7-: &f-%s").format(like.x(), like.y(), like.z()).colored(),
-							"",
-							Text.of("&7-クリック: &a-下記の機能の選択画面に移行します！").colored(),
-							Text.of("&7-: &a-このLikeにテレポートする！(&n-%sMP-&r&a)").format(config.teleportationCosts()).colored(),
-							Text.of("&7-: &a-半径%sm以内にいるプレイヤーをこのLikeに招待する！(&n-%sMP-&r&a)").format(config.radiusOfInvitationScope(), config.invitationCosts()).colored()
+							Text.of("&7-%s").apply(like.description()),
+							Text.empty(),
+							Text.of("&7-作成者: &a-%s").apply(UUIDConverter.getNameFromUUID(like.owner())),
+							Text.of("&7-お気に入り数: &a-%s").apply(like.favorites()),
+							Text.of("&7-作成日時: &a-%s").apply(like.creationTimestamp()),
+							Text.of("&7-ワールド: &a-%s").apply(config.alias(like.world()).or(() -> "Unknown")),
+							Text.of("&a-座標-&7-: &f-X-&7-: &f-%s Y-&7-: &f-%s Z-&7-: &f-%s").apply(like.x(), like.y(), like.z()),
+							Text.empty(),
+							Text.of("&7-クリック: &a-下記の機能の選択画面に移行します！"),
+							Text.of("&7-: &a-このLikeにテレポートする！(&n-%sMP-&r&a)").apply(config.teleportationCosts()),
+							Text.of("&7-: &a-半径%sm以内にいるプレイヤーをこのLikeに招待する！(&n-%sMP-&r&a)").apply(config.radiusOfInvitationScope(), config.invitationCosts())
 						);
 					});
+					
 					s.onClick(e -> new TeleportationTypeSelectionUI(like).open(p));
 				}, slotIndex);
 			});
 			
-			if(index > 0){
+			if(where != Type.FIRST){
 				l.put(s -> {
 					s.icon(i -> {
 						i.basedItemStack = Skull.createFrom("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYmQ2OWUwNmU1ZGFkZmQ4NGU1ZjNkMWMyMTA2M2YyNTUzYjJmYTk0NWVlMWQ0ZDcxNTJmZGM1NDI1YmMxMmE5In19fQ==");
-						i.displayName = Text.of("&a-前のページに戻る").colored();
+						i.displayName = Text.of("&a-前のページに戻る").get();
 					});
+					
 					s.onClick(e -> {
 						index--;
 						open(p);
@@ -107,12 +112,13 @@ public class MyLikeManagementUI implements InventoryUI {
 				});
 			}, 49);
 			
-			if(index < likes.size() / 45 + (likes.size() % 45 == 0 ? -1 : 0)){
+			if(where != Type.LAST){
 				l.put(s -> {
 					s.icon(i -> {
 						i.basedItemStack = Skull.createFrom("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMTliZjMyOTJlMTI2YTEwNWI1NGViYTcxM2FhMWIxNTJkNTQxYTFkODkzODgyOWM1NjM2NGQxNzhlZDIyYmYifX19");
-						i.displayName = Text.of("&a-次のページに進む").colored();
+						i.displayName = Text.of("&a-次のページに進む").get();
 					});
+					
 					s.onClick(e -> {
 						index++;
 						open(p);
@@ -147,12 +153,11 @@ public class MyLikeManagementUI implements InventoryUI {
 		public final Comparator<Like> comparator;
 		
 		private Order(Function<Like, Integer> function){
-			Comparator<Like> comparator = Comparator.comparing(function);
-			this.comparator = ordinal() % 2 == 0 ? comparator.reversed() : comparator;
+			this(Comparator.comparing(function));
 		}
 		
 		private Order(Comparator<Like> comparator){
-			this.comparator = comparator;
+			this.comparator = ordinal() % 2 == 0 ? comparator.reversed() : comparator;
 		}
 		
 	}
