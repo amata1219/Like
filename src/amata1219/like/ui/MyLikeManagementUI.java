89e451b6ce37d6459c2afdf603b531cbbf7920d5
amata1219 <suzukiamata@gmail.com>
2019-12-15 09:36:10 +0900

    こみっとー

--- src/amata1219/like/ui/MyLikeManagementUI.java
+++ src/amata1219/like/ui/MyLikeManagementUI.java
@@ -10,7 +10,6 @@ import org.bukkit.entity.Player;
 import amata1219.like.Like;
 import amata1219.like.config.MainConfig.IconType;
 import amata1219.masquerade.dsl.component.Layout;
-import amata1219.masquerade.item.Skull;
 import amata1219.masquerade.text.Text;
 import at.pcgamingfreaks.UUIDConverter;
 
@@ -25,7 +24,7 @@ public class MyLikeManagementUI extends AbstractMultipleUI {
 	@Override
 	public Function<Player, Layout> layout() {
 		final ArrayList<Like> likes = new ArrayList<>(plugin.likes(owner));
-		final Type where = Type.where(index, likes);
+		final Type type = Type.where(index, likes);
 		likes.sort(order.comparator);
 		return build(Math.min(likes.size() - (index * 45), 45), (p, l) -> {
 			l.title = Text.of("自分のLike @ %s").format(index + 1);
@@ -65,135 +64,6 @@ public class MyLikeManagementUI extends AbstractMultipleUI {
 				}, slotIndex);
 			});
 			
-			if(where == Type.FIRST){
-				l.put(s -> {
-					s.icon(i -> {
-						i.basedItemStack = Skull.createFrom("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZWVkNzg4MjI1NzYzMTdiMDQ4ZWVhOTIyMjdjZDg1ZjdhZmNjNDQxNDhkY2I4MzI3MzNiYWNjYjhlYjU2ZmExIn19fQ==");
-						i.displayName = Text.color("&c-これ以上前には戻れません");
-					});
-				}, 45);
-			}else{
-				l.put(s -> {
-					s.icon(i -> {
-						i.basedItemStack = Skull.createFrom("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYmQ2OWUwNmU1ZGFkZmQ4NGU1ZjNkMWMyMTA2M2YyNTUzYjJmYTk0NWVlMWQ0ZDcxNTJmZGM1NDI1YmMxMmE5In19fQ==");
-						i.displayName = Text.color("&a-前のページに戻る");
-					});
-					
-					s.onClick(e -> {
-						index--;
-						open(p);
-					});
-				}, 45);
-			}
-			
-			if(where == Type.LAST){
-				l.put(s -> {
-					s.icon(i -> {
-						i.basedItemStack = Skull.createFrom("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNzE1NDQ1ZGExNmZhYjY3ZmNkODI3ZjcxYmFlOWMxZDJmOTBjNzNlYjJjMWJkMWVmOGQ4Mzk2Y2Q4ZTgifX19");
-						i.displayName = Text.color("&c-これ以上次には進めません");
-					});
-				}, 53);
-			}else{
-				l.put(s -> {
-					s.icon(i -> {
-						i.basedItemStack = Skull.createFrom("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMTliZjMyOTJlMTI2YTEwNWI1NGViYTcxM2FhMWIxNTJkNTQxYTFkODkzODgyOWM1NjM2NGQxNzhlZDIyYmYifX19");
-						i.displayName = Text.color("&a-次のページに進む");
-					});
-					
-					s.onClick(e -> {
-						index++;
-						open(p);
-					});
-				}, 53);
-			}
-			
-			if(order == Order.CREATION_DATE_IN_DESCENDING){
-				l.put(s -> {
-					s.icon(i -> {
-						i.material = config.material(IconType.SORT_BY_CREATION_DATE_IN_DESCENDING_ORDER);
-						i.displayName = Text.color("&a-作成日が新しい順に表示されています！");
-						i.gleam();
-					});
-				}, 47);
-			}else{
-				l.put(s -> {
-					s.icon(i -> {
-						i.material = config.material(IconType.SORT_BY_CREATION_DATE_IN_DESCENDING_ORDER);
-						i.displayName = Text.color("&7-作成日が新しい順に並び替える");
-					});
-					
-					s.onClick(e -> {
-						order = Order.CREATION_DATE_IN_DESCENDING;
-						open(p);
-					});
-				}, 47);
-			}
-			
-			if(order == Order.CREATION_DATE_IN_ASCENDING){
-				l.put(s -> {
-					s.icon(i -> {
-						i.material = config.material(IconType.SORT_BY_CREATION_DATE_IN_ASCENDING_ORDER);
-						i.displayName = Text.color("&a-作成日が古い順に表示されています！");
-						i.gleam();
-					});
-				}, 48);
-			}else{
-				l.put(s -> {
-					s.icon(i -> {
-						i.material = config.material(IconType.SORT_BY_CREATION_DATE_IN_ASCENDING_ORDER);
-						i.displayName = Text.color("&7-作成日が古い順に並び替える");
-					});
-					
-					s.onClick(e -> {
-						order = Order.CREATION_DATE_IN_ASCENDING;
-						open(p);
-					});
-				}, 48);
-			}
-			
-			if(order == Order.FAVORITES_IN_DESCENDING){
-				l.put(s -> {
-					s.icon(i -> {
-						i.material = config.material(IconType.SORT_BY_FAVORITES_IN_DESCENDING_ORDER);
-						i.displayName = Text.color("&a-お気に入り数が多い順に表示されています！");
-						i.gleam();
-					});
-				}, 50);
-			}else{
-				l.put(s -> {
-					s.icon(i -> {
-						i.material = config.material(IconType.SORT_BY_FAVORITES_IN_DESCENDING_ORDER);
-						i.displayName = Text.color("&7-お気に入り数が多い順に並び替える");
-					});
-					
-					s.onClick(e -> {
-						order = Order.FAVORITES_IN_DESCENDING;
-						open(p);
-					});
-				}, 50);
-			}
-			
-			if(order == Order.FAVORITES_IN_ASCENDING){
-				l.put(s -> {
-					s.icon(i -> {
-						i.material = config.material(IconType.SORT_BY_FAVORITES_IN_ASCENDING_ORDER);
-						i.displayName = Text.color("&a-お気に入り数が少ない順に表示されています！");
-						i.gleam();
-					});
-				}, 51);
-			}else{
-				l.put(s -> {
-					s.icon(i -> {
-						i.material = config.material(IconType.SORT_BY_FAVORITES_IN_ASCENDING_ORDER);
-						i.displayName = Text.color("&7-お気に入り数が少ない順に並び替える");
-					});
-					
-					s.onClick(e -> {
-						order = Order.FAVORITES_IN_ASCENDING;
-						open(p);
-					});
-				}, 51);
-			}
 			
 		});
 	}
