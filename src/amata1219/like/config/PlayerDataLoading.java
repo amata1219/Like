3fc2cf49d8d7e8b89e5728d61ad19ebc1e0684f4
amata1219 <suzukiamata@gmail.com>
2019-12-20 13:04:40 +0900

    こみっとー

--- src/amata1219/like/config/PlayerDataLoading.java
+++ src/amata1219/like/config/PlayerDataLoading.java
@@ -2,6 +2,7 @@ package amata1219.like.config;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
@@ -24,11 +25,23 @@ public class PlayerDataLoading extends Yaml {
 		throw new NotImplementedException();
 	}
 	
-	public HashMap<UUID, PlayerData> loadExistingAllPlayerData(){
+	public HashMap<UUID, PlayerData> load(HashMap<UUID, List<Like>> playerLikes){
+		HashMap<UUID, PlayerData> players = new HashMap<>();
+		for(String path : getKeys(false)){
+			PlayerData data = new PlayerData();
+			UUID uuid = UUID.fromString(path);
+			playerLikes.getOrDefault(uuid, Collections.emptyList()).forEach(data::addLike);
+			Arrays.stream(getString(uuid.toString()).split(","))
+				.map(Long::valueOf)
+				.map(Main.plugin().likes::get)
+				.forEach(data::addFavoriteLike);
+			players.put(uuid, data);
+		}
+		return players;
 	}
 	
 	public PlayerData loadExistingPlayerData(UUID uuid){
-		
+		PlayerData data = new PlayerData();
 	}
 	
 	public Collection<Like> favoriteLikes(UUID uuid){
--- src/amata1219/like/player/PlayerData.java
+++ src/amata1219/like/player/PlayerData.java
@@ -8,4 +8,12 @@ public class PlayerData {
 	
 	public final HashMap<Long, Like> likes = new HashMap<>(), favoriteLikes = new HashMap<>();
 	
+	public void addLike(Like like){
+		likes.put(like.id, like);
+	}
+	
+	public void addFavoriteLike(Like like){
+		favoriteLikes.put(like.id, like);
+	}
+	
 }
