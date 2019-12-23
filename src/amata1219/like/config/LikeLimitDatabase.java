3a1ac1a9ef99ef51378ed5807b1c740b76e9a66c
amata1219 <suzukiamata@gmail.com>
2019-12-23 19:55:43 +0900

    こみっとー

--- src/amata1219/like/config/LikeLimitDatabase.java
+++ src/amata1219/like/config/LikeLimitDatabase.java
@@ -2,27 +2,31 @@ package amata1219.like.config;
 
 import java.util.UUID;
 
+import org.bukkit.configuration.file.FileConfiguration;
+
 import amata1219.like.exception.NotImplementedException;
 
-public class LikeLimitDatabase extends Yaml {
+public class LikeLimitDatabase extends Config {
 
 	public LikeLimitDatabase() {
 		super("like_limit.yml");
 	}
 
 	@Override
-	public void readAll() {
+	public void load() {
 		throw new NotImplementedException();
 	}
 	
 	public int limit(UUID uuid){
+		FileConfiguration config = config();
 		String path = uuid.toString();
-		if(!contains(path)) set(path, getInt("Default"));
-		return getInt(path);
+		if(!config.contains(path)) config.set(path, config.getInt("Default"));
+		return config.getInt(path);
 	}
 	
 	public void set(UUID uuid, int limit){
-		set(uuid.toString(), limit);
+		FileConfiguration config = config();
+		config.set(uuid.toString(), limit);
 		update();
 	}
 
