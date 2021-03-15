95873a89a52a4a9d7fbbea0c97dba9ac6577c6bb
amata1219 <suzukiamata@gmail.com>
2021-03-15 18:24:34 +0900

    SEを追加

--- src/amata1219/like/sound/SoundEffects.java
+++ src/amata1219/like/sound/SoundEffects.java
@@ -6,4 +6,6 @@ public class SoundEffects {
 
     public static final SoundEffect OPEN_BOOKMARK_UI = new SoundEffect(BLOCK_CHEST_OPEN, 1f, 1f);
 
+    public static final SoundEffect EXECUTE_LIKERD_COMMAND = new SoundEffect(ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 1.6f);
+
 }
