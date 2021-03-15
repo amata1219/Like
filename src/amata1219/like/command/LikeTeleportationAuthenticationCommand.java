6a85b2caf779b43bb50e1933aea0e4b444f0307d
amata1219 <suzukiamata@gmail.com>
2021-03-15 21:18:09 +0900

    リファクタリング

--- src/amata1219/like/command/LikeTeleportationAuthenticationCommand.java
+++ src/amata1219/like/command/LikeTeleportationAuthenticationCommand.java
@@ -1,30 +1,42 @@
 package amata1219.like.command;
 
-import org.bukkit.command.CommandExecutor;
-
+import amata1219.bryionake.constant.CommandSenderCasters;
+import amata1219.bryionake.constant.Parsers;
+import amata1219.bryionake.dsl.BukkitCommandExecutor;
+import amata1219.bryionake.dsl.context.CommandContext;
 import amata1219.like.Like;
 import amata1219.like.Main;
-import amata1219.like.slash.builder.ContextualExecutorBuilder;
-import amata1219.like.slash.builder.Parser;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-public class LikeTeleportationAuthenticationCommand {
-	
-	public static final CommandExecutor executor = ContextualExecutorBuilder.playerCommandBuilder()
-			.parsers(
-				() -> "You don't have like.like",
-				Parser.identity(),
-				ParserTemplates.like()
-			)
-			.execution(context -> sender -> {
-				String token = context.arguments.parsed(0);
-				if(!token.equals(Main.INVITATION_TOKEN)){
-					sender.sendMessage("You don't have like.like");
-					return;
-				}
-				
-				Like like = context.arguments.parsed(1);
-				sender.teleport(like.hologram.getLocation());
-				Main.plugin().config().teleportationText().apply(like).sendTo(sender);
-			}).build();
+public class LikeTeleportationAuthenticationCommand implements BukkitCommandExecutor {
+
+	private final CommandContext<CommandSender> executor;
+
+	{
+		CommandContext<Player> authenticate = define(
+				() -> "",
+				(sender, unparsedArguments, parsedArguments) -> {
+					String token = parsedArguments.poll();
+					if(!token.equals(Main.INVITATION_TOKEN)){
+						sender.sendMessage("You don't have like.like");
+						return;
+					}
+
+					Like like = parsedArguments.poll();
+					sender.teleport(like.hologram.getLocation());
+					Main.plugin().config().teleportationText().apply(like).sendTo(sender);
+				},
+				Parsers.str,
+				ParserTemplates.like
+		);
+
+		executor = define(CommandSenderCasters.casterToPlayer, authenticate);
+	}
+
+	@Override
+	public CommandContext<CommandSender> executor() {
+		return executor;
+	}
 
 }
