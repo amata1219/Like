54ba30f22376c629f5a1086d90a36b2177f8316e
amata1219 <suzukiamata@gmail.com>
2021-03-16 19:51:13 +0900

    ツアー用の定期告知機能を追加

---
+++ src/amata1219/like/task/TourRegularNotificationTask.java
@@ -0,0 +1,30 @@
+package amata1219.like.task;
+
+import amata1219.like.config.TourConfig;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class TourRegularNotificationTask implements Runnable {
+
+    private final TourConfig config;
+    private final TextComponent message;
+
+    public TourRegularNotificationTask(TourConfig config) {
+        this.config = config;
+        this.message = new TextComponent(config.notificationMessage());
+        message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/like tour"));
+        message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(ChatColor.GREEN + "クリックで /like tour コマンドを実行します。")));
+
+    }
+
+    @Override
+    public void run() {
+        for (Player player : Bukkit.getOnlinePlayers()) player.spigot().sendMessage(message);
+    }
+
+}
