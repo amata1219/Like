7b11f11d4b92f2571d99b7e1177d90c55fd82e8d
amata1219 <suzukiamata@gmail.com>
2021-03-17 13:43:31 +0900

    テレポートから一定時間経過後にガイドメッセージを表示する昨日を追加

--- src/amata1219/like/ui/LikeTourTpConfirmationUI.java
+++ src/amata1219/like/ui/LikeTourTpConfirmationUI.java
@@ -3,20 +3,27 @@ package amata1219.like.ui;
 import amata1219.like.Like;
 import amata1219.like.Main;
 import amata1219.like.config.MainConfig;
+import amata1219.like.config.TourConfig;
 import amata1219.like.masquerade.dsl.InventoryUI;
 import amata1219.like.masquerade.dsl.component.Layout;
 import amata1219.like.masquerade.option.Lines;
 import amata1219.like.masquerade.text.Text;
+import amata1219.like.task.TaskRunner;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
+import java.awt.*;
 import java.util.Comparator;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
-public class TeleportationConfirmationUI implements InventoryUI {
+public class LikeTourTpConfirmationUI implements InventoryUI {
 
     private final Main plugin = Main.plugin();
     private final Economy economy = plugin.economy();
@@ -24,7 +31,7 @@ public class TeleportationConfirmationUI implements InventoryUI {
     private final Like like;
     private final InventoryUI previous;
 
-    public TeleportationConfirmationUI(Like like, InventoryUI previous){
+    public LikeTourTpConfirmationUI(Like like, InventoryUI previous){
         this.like = like;
         this.previous = previous;
     }
@@ -74,6 +81,14 @@ public class TeleportationConfirmationUI implements InventoryUI {
                     economy.depositPlayer(Bukkit.getOfflinePlayer(like.owner()), config.teleportationCosts());
                     p.teleport(like.hologram.getLocation());
                     config.teleportationText().apply(like).accept(p::sendMessage);
+
+                    TourConfig config = Main.plugin().tourConfig();
+                    TaskRunner.runTaskLaterAsynchronously(task -> {
+                        TextComponent message = new TextComponent(config.guideMessage());
+                        message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/like tour"));
+                        message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new net.md_5.bungee.api.chat.hover.content.Text(ChatColor.GREEN + "クリックで /like tour コマンドを実行しツアー専用UIを開きます！")));
+                        p.spigot().sendMessage(message);
+                    }, config.guideDelayTicks());
                 });
             }, 4);
 
