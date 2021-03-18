877aa63270054294a481a81df39a9c4efe11c308
amata1219 <suzukiamata@gmail.com>
2021-03-18 18:31:32 +0900

    Likeの範囲検索機能用のUIを実装

---
+++ src/amata1219/like/ui/LikeRangeSearchTpConfirmationUI.java
@@ -0,0 +1,105 @@
+package amata1219.like.ui;
+
+import amata1219.like.Like;
+import amata1219.like.Main;
+import amata1219.like.config.MainConfig;
+import amata1219.like.listener.ControlLikeViewListener;
+import amata1219.like.masquerade.dsl.InventoryUI;
+import amata1219.like.masquerade.dsl.component.Layout;
+import amata1219.like.masquerade.option.Lines;
+import amata1219.like.masquerade.text.Text;
+import amata1219.like.sound.SoundEffects;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+import java.util.function.Function;
+
+public class LikeRangeSearchTpConfirmationUI implements InventoryUI {
+
+    private final Main plugin = Main.plugin();
+    private final Economy economy = plugin.economy();
+    private final MainConfig config = plugin.config();
+    private final Like like;
+    private final InventoryUI previous;
+
+    public LikeRangeSearchTpConfirmationUI(Like like, InventoryUI previous){
+        this.like = like;
+        this.previous = previous;
+    }
+
+    @Override
+    public Function<Player, Layout> layout() {
+        return build(Lines.x1, (player, l) -> {
+            l.title = "テレポートの実行確認画面";
+
+            l.defaultSlot(s -> s.icon(i -> {
+                i.material = Material.LIGHT_GRAY_STAINED_GLASS_PANE;
+                i.displayName = " ";
+            }));
+
+            l.put(s -> s.icon(i -> {
+                i.material = config.material(MainConfig.IconType.LIKE);
+                i.displayName = Text.of("&a&l-%s").format(like.id);
+                i.amount = Math.min(Math.max(like.favorites(), 1), 64);
+                i.lore(
+                        Text.of("&7-%s").format(like.description()),
+                        "",
+                        Text.of("&7-作成者: &a-%s").format(like.ownerName()),
+                        Text.of("&7-お気に入り数: &a-%s").format(like.favorites()),
+                        Text.of("&7-作成日時: &a-%s").format(like.creationTimestamp()),
+                        Text.of("&7-ワールド: &a-%s").format(config.worldAlias(like.world())),
+                        Text.of("&7-座標: &a-X-&7-: &a-%s Y-&7-: &a-%s Z-&7-: &a-%s").format(like.x(), like.y(), like.z())
+                );
+            }), 1);
+
+            l.put(s -> {
+                s.icon(i -> {
+                    i.material = config.material(MainConfig.IconType.TELEPORT_TO_LIKE);
+                    i.displayName = Text.of("&a-このLikeにテレポートする！ (%sMP)").format(config.teleportationCosts());
+                });
+
+                s.onClick(e -> {
+                    double costs = config.teleportationCosts();
+                    if(!economy.has(player, costs)){
+                        Text.of("&c-所持金が足りません。テレポートするには%sMP必要です。").apply(costs).sendTo(player);
+                        return;
+                    }
+                    economy.withdrawPlayer(player, costs);
+                    economy.depositPlayer(Bukkit.getOfflinePlayer(like.owner()), config.teleportationCosts());
+
+                    ControlLikeViewListener listener = plugin.controlLikeViewListener;
+                    listener.viewersToRespawnPoints.remove(player);
+                    listener.disableViewingMode(player);
+
+                    player.closeInventory();
+
+                    SoundEffects.TELEPORTED_TO_LIKE.play(player);
+                    config.teleportationText().apply(like).accept(player::sendMessage);
+                });
+            }, 6);
+
+            l.put(s -> {
+                s.icon(i -> {
+                    i.material = config.material(MainConfig.IconType.CANCEL_LIKE_TELEPORTATION);
+                    i.displayName = Text.color("&c-確認をやめて元の場所に戻る");
+                });
+
+                s.onClick(e -> {
+                    ControlLikeViewListener listener = plugin.controlLikeViewListener;
+
+                    listener.disableViewingMode(player);
+
+                    Location respawnPoint = listener.viewersToRespawnPoints.remove(player);
+                    player.teleport(respawnPoint);
+
+                    previous.open(player);
+                });
+            }, 7);
+
+        });
+    }
+
+}
---
+++ src/amata1219/like/ui/LikeRangeSearchingUI.java
@@ -0,0 +1,71 @@
+package amata1219.like.ui;
+
+import amata1219.like.Like;
+import amata1219.like.config.MainConfig;
+import amata1219.like.listener.ControlLikeViewListener;
+import amata1219.like.masquerade.dsl.component.Layout;
+import amata1219.like.masquerade.text.Text;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LikeRangeSearchingUI extends AbstractSortableLikeListUI {
+
+    private final List<Like> likes;
+
+    public LikeRangeSearchingUI(List<Like> likes) {
+        super();
+        this.likes = likes;
+    }
+    @Override
+    protected List<Like> likes() {
+        return likes;
+    }
+
+    @Override
+    protected void layout(Player player, Layout l, List<Like> likes) {
+        super.layout(player, l, likes);
+
+        l.title = Text.of("周辺のLike一覧 @ %sページ目").format(index + 1);
+
+        int remainder = likes.size() - (index * 45) >= 45 ? 45 : likes.size() % 45;
+        IntStream.range(0, remainder != 0 ? remainder : likes.isEmpty() ? 0 : 45).forEach(slotIndex -> {
+            l.put(s -> {
+                final Like like = likes.get(slotIndex);
+                s.icon(i -> {
+                    i.material = config.material(MainConfig.IconType.LIKE);
+                    i.displayName = Text.of("&a&l-%s").format(like.id);
+                    i.amount = Math.min(Math.max(like.favorites(), 1), 64);
+                    i.lore(
+                            Text.of("&7-%s").format(like.description()),
+                            "",
+                            Text.of("&7-お気に入り数: &a-%s").format(like.favorites()),
+                            Text.of("&7-作成日時: &a-%s").format(like.creationTimestamp()),
+                            Text.of("&7-ワールド: &a-%s").format(config.worldAlias(like.world())),
+                            Text.of("&7-座標: &a-X-&7-: &a-%s Y-&7-: &a-%s Z-&7-: &a-%s").format(like.x(), like.y(), like.z()),
+                            "",
+                            Text.color("&7-クリック: Likeのある場所に視点が移動します。")
+                    );
+                });
+
+                s.onClick(e -> {
+                    player.teleport(like.hologram.getLocation());
+                    ControlLikeViewListener listener = plugin.controlLikeViewListener;
+                    listener.viewersToRespawnPoints.put(player, player.getLocation());
+                    listener.viewersToLikesViewed.put(player, like);
+                    listener.viewersToUIs.put(player, this);
+
+                    TextComponent component = new TextComponent(ChatColor.GREEN + "[Like]: テレポートの確定・キャンセルはこちらをクリック！ 専用GUIが開きます！");
+                    component.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/likeorscu"));
+                    component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new net.md_5.bungee.api.chat.hover.content.Text("左クリックで専用GUIを表示")));
+                    player.spigot().sendMessage(component);
+                });
+            }, slotIndex);
+        });
+    }
+}
