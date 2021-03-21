9b92ef32f7913b89a1865e88a4827cb97d1f5ec5
amata1219 <suzukiamata@gmail.com>
2021-03-21 12:03:28 +0900

    SEを追加

--- src/amata1219/like/sound/SoundEffects.java
+++ src/amata1219/like/sound/SoundEffects.java
@@ -6,10 +6,12 @@ public class SoundEffects {
 
     public static final SoundEffect OPEN_INVENTORY_UI = new SoundEffect(BLOCK_CHEST_OPEN, 1f, 1f);
 
-    public static final SoundEffect SELECT_RANDOM_LIKE = new SoundEffect(ENTITY_PLAYER_LEVELUP, 1f, 1.2f);
+    public static final SoundEffect OPERATED = new SoundEffect(ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 2.0f);
+
+    public static final SoundEffect PREPARED = new SoundEffect(ENTITY_PLAYER_LEVELUP, 1f, 1.2f);
 
     public static final SoundEffect SUCCEEDED = new SoundEffect(ENTITY_PLAYER_LEVELUP, 1f, 1.8f);
 
-    public static final SoundEffect OPERATION = new SoundEffect(ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 2.0f);
+    public static final SoundEffect FAILED = new SoundEffect(BLOCK_CHAIN_BREAK, 1f, 1.8f);
 
 }
