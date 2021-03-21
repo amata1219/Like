0456e9e67b4e446bf852a8fe7111ac025b08661b
amata1219 <suzukiamata@gmail.com>
2021-03-21 21:14:05 +0900

    ツアーの告知が行われていない時はツアー専用GUIを開けないように変更

--- src/amata1219/like/command/LikeTourCommand.java
+++ src/amata1219/like/command/LikeTourCommand.java
@@ -1,16 +1,27 @@
 package amata1219.like.command;
 
+import amata1219.like.Main;
 import amata1219.like.bryionake.constant.CommandSenderCasters;
 import amata1219.like.bryionake.dsl.BukkitCommandExecutor;
 import amata1219.like.bryionake.dsl.context.CommandContext;
+import amata1219.like.sound.SoundEffects;
 import amata1219.like.ui.TourLikeListUI;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class LikeTourCommand implements BukkitCommandExecutor {
 
     public static final LikeTourCommand INSTANCE = new LikeTourCommand();
 
-    private final CommandContext<CommandSender> executor = define(CommandSenderCasters.casterToPlayer, (sender, unparsedArguments, parsedArguments) -> new TourLikeListUI().open(sender));
+    private final CommandContext<CommandSender> executor = define(CommandSenderCasters.casterToPlayer, (sender, unparsedArguments, parsedArguments) -> {
+        if (!Main.plugin().tourConfig().notificationIsEnabled()) {
+            sender.sendMessage(ChatColor.RED + "ツアーが現在開催されていないため専用GUIを開くことができません。");
+            SoundEffects.FAILED.play(sender);
+            return;
+        }
+
+        new TourLikeListUI().open(sender);
+    });
 
     @Override
     public CommandContext<CommandSender> executor() {
